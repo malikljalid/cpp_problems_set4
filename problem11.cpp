8fe7d73e2b0d55a6894fca3eb3ab2af6ea8549c8
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-09 23:36:14 +0100

    converte amount of days from start of year to date mm-dd-yy

---
+++ problem11.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+struct stDate
+{
+    short int Day;
+    short int Month;
+    short int Year;
+};
+
+int readNumber(std::string msg)
+{
+    int Number;
+
+    std::cout << "Please enter a " << msg << " : ";
+    std::cin >> Number;
+
+    return (Number);
+}
+
+short int last2DigitsOf(int Number)
+{
+    return (Number % 100);
+}
+
+short int first2DigitsOf(int Number)
+{
+    return (Number / 100);
+}
+
+bool leapYear(short int year)
+{
+    return ((year % 4 == 0) ? ((year % 100 == 0) ? ((year % 400 == 0) ? true : false ) : true) : false);
+}
+
+short int getMonthDays(short int Month, short int Year)
+{
+    short int listOfDays[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    return ((Month == 2) ? (leapYear(Year) ? 29 : 28) : listOfDays[Month]); 
+}
+
+short int getNumberOfDaysFromStartOf(short int Day, short int Month, short int Year)
+{
+    short int Days = Day;
+
+    for (short int i = 1; i <= Month; i++)
+        Days += getMonthDays(i, Year);
+
+    return (Days);
+}
+
+stDate getDayMonthYearFromSumOfDaysFromStartOfYear(short Year, short int DaysFromStartOfYear)
+{
+    stDate      Date;
+    short int   Month = 0;
+
+    for (Month = 1; Month <= 12; Month++)
+    {
+        DaysFromStartOfYear -= getMonthDays(Month, Year);
+
+        if (DaysFromStartOfYear < getMonthDays(Month+1, Year))
+            break;
+    }
+
+    Date.Day    = DaysFromStartOfYear;
+    Date.Month  = Month;
+    Date.Year   = Year;
+
+    return (Date); 
+}
+
+void printNumberOfDaysFromStartOf(short int Day, short int Month, short int Year)
+{
+    std::cout << "\nNumber of days form the beggining of " << Year << " is : " << getNumberOfDaysFromStartOf(Day, Month, Year) << std::endl;
+}
+
+void printDateFromGivenDaysFromStartOfYear(int Year, int DaysFromStartOfYear)
+{
+    stDate Date = getDayMonthYearFromSumOfDaysFromStartOfYear(Year, DaysFromStartOfYear);
+
+    std::cout << "Date : " << Date.Day << "/" << Date.Month << "/" << Date.Year << "\n";
+}
+
+int main(void)
+{
+    short int Year  = readNumber("Year");
+    short int Month = readNumber("Month");
+    short int Day   = readNumber("Day");
+
+    printNumberOfDaysFromStartOf(Day, Month, Year);
+    printDateFromGivenDaysFromStartOfYear(Year, getNumberOfDaysFromStartOf(Day, Month, Year));
+
+    return (0);
+}
