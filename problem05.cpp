d5fc0dcf5e2fa35439e2d5b3d65b1098d0511d10
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-09 13:07:04 +0100

    print number of days, hours, minutesand seconds in a month

---
+++ problem05.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int readNumber(std::string msg)
+{
+    int Number;
+
+    std::cout << "Please enter a " << msg << " to check : ";
+    std::cin >> Number;
+
+    return (Number);
+}
+
+bool leapYear(short int year)
+{
+    return ((year % 4 == 0) ? ((year % 100 == 0) ? ((year % 400 == 0) ? true : false ) : true) : false);
+}
+
+short int getMonthDays(short int Month, short int Year)
+{
+    short int listOf31DaysMonths[13] = { 0, 1, 0, 3, 0, 5, 0, 7, 8, 0, 10, 0, 12 };
+
+    if (Month == 2)
+        return (leapYear(Year) ? 28 : 29);
+    return (listOf31DaysMonths[Month] == 0 ? 30 : 31); 
+}
+
+void printMonthData(short int Month, short int Year)
+{
+    std::string _months[13] = { "", "January", "February", "March", "April", "Mai", "June", "July", "Aghust", "September", "October", "Novemeber", "December" };
+
+    int Days    = getMonthDays(Month, Year);
+    int Hours   = Days * 24;
+    int Minutes = Hours * 60;
+    int Seconds = Minutes * 60;
+
+    std::cout << std::endl;
+    std::cout << "Days in " << _months[Month] << " : " << Days << std::endl;
+    std::cout << "Hours in " << _months[Month] << " : " << Hours << std::endl;
+    std::cout << "Minutes in " << _months[Month] << " : " << Minutes << std::endl;
+    std::cout << "Seconds in " << _months[Month] << " : " << Seconds << std::endl;
+}
+
+int main(void)
+{
+    printMonthData(readNumber("Month"), readNumber("Year"));
+
+    return (0);
+}
