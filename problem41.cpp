ef4809aa439782da663939de2cd6715e2bcb5835
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-11 16:21:58 +0100

    decrease date by X years.. but FASTER

---
+++ problem41.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+struct stDate
+{
+    short int Day;
+    short int Month;
+    short int Year;
+};
+
+stDate initDate(short int Day, short int Month, short int Year)
+{
+    stDate Date;
+
+    Date.Day    =   Day;
+    Date.Month  =   Month;
+    Date.Year   =   Year;
+
+    return (Date);
+}
+
+void printDate(stDate Date)
+{
+    std::cout << Date.Day << "/" << Date.Month << "/" << Date.Year << std::endl;
+}
+
+int readNumber(std::string msg)
+{
+    int Number;
+
+    std::cout << msg;
+    std::cin >> Number;
+
+    return (Number);
+}
+
+stDate decreaseDateByXYears(stDate Date, short int YearsToSub)
+{
+    Date.Year -= YearsToSub;
+
+    return (Date);
+}
+
+int main(void)
+{
+    short int Year  = readNumber(" Please enter a Year  : ");
+    short int Month = readNumber(" Please enter a Month : ");
+    short int Day   = readNumber(" Please enter a Day   : ");
+
+
+    stDate Date = initDate(Day, Month, Year);
+
+    printDate(Date);
+    printDate(decreaseDateByXYears(Date, readNumber("How many years you want to sub ? : ")));
+
+    return (0);
+}
