8f25847ab5bdb4df27446aee94c82475cdf64a70
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-11 16:03:32 +0100

    decrease date by X months

---
+++ problem38.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+struct stDate
+{
+    short int Day;
+    short int Month;
+    short int Year;
+};
+
+stDate initDate(short int Day, short int Month, short int Year)
+{
+    stDate Date;
+
+    Date.Day    =   Day;
+    Date.Month  =   Month;
+    Date.Year   =   Year;
+
+    return (Date);
+}
+
+void printDate(stDate Date)
+{
+    std::cout << Date.Day << "/" << Date.Month << "/" << Date.Year << std::endl;
+}
+
+int readNumber(std::string msg)
+{
+    int Number;
+
+    std::cout << msg;
+    std::cin >> Number;
+
+    return (Number);
+}
+
+bool leapYear(short int year)
+{
+    return ((year % 4 == 0) ? ((year % 100 == 0) ? ((year % 400 == 0) ? true : false ) : true) : false);
+}
+
+short int getMonthDays(short int Month, short int Year)
+{
+    short int listOfDays[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    return ((Month == 2) ? (leapYear(Year) ? 29 : 28) : listOfDays[Month]); 
+}
+
+stDate decreaseDateByOneMonth(stDate Date)
+{
+    if (Date.Month == 1)
+    {
+        Date.Year--;
+        Date.Month = 12;
+    }
+    else
+        Date.Month--;
+
+    if (Date.Day > getMonthDays(Date.Month, Date.Year))
+        Date.Day = getMonthDays(Date.Month, Date.Year);
+
+    return (Date);
+}
+
+stDate decreaseDateByXMonths(stDate Date, short int MonthsToSub)
+{
+    while (MonthsToSub--)
+        Date = decreaseDateByOneMonth(Date);
+
+    return (Date);
+}
+
+int main(void)
+{
+    short int Year  = readNumber(" Please enter a Year  : ");
+    short int Month = readNumber(" Please enter a Month : ");
+    short int Day   = readNumber(" Please enter a Day   : ");
+
+
+    stDate Date = initDate(Day, Month, Year);
+
+    printDate(Date);
+    printDate(decreaseDateByXMonths(Date, readNumber(" Please enter months number to sub ? : ")));
+
+    return (0);
+}
