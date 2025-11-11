291587b4f07dc6a234121d1a01bc5063c7a32e94
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-11 16:32:21 +0100

    decrease date by X decades

---
+++ problem43.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+struct stDate
+{
+    short int Day;
+    short int Month;
+    short int Year;
+};
+
+stDate initDate(short int Day, short int Month, short int Year)
+{
+    stDate Date;
+
+    Date.Day    =   Day;
+    Date.Month  =   Month;
+    Date.Year   =   Year;
+
+    return (Date);
+}
+
+void printDate(stDate Date)
+{
+    std::cout << Date.Day << "/" << Date.Month << "/" << Date.Year << std::endl;
+}
+
+int readNumber(std::string msg)
+{
+    int Number;
+
+    std::cout << msg;
+    std::cin >> Number;
+
+    return (Number);
+}
+
+stDate decreaseDateByOneDecade(stDate Date)
+{
+    Date.Year -= 10;
+
+    return (Date);
+}
+
+stDate decreaseDateByXDecades(stDate Date, short int DecadesToSub)
+{
+    while (DecadesToSub--)
+        Date = decreaseDateByOneDecade(Date);
+    
+    return (Date);
+}
+
+int main(void)
+{
+    short int Year  = readNumber(" Please enter a Year  : ");
+    short int Month = readNumber(" Please enter a Month : ");
+    short int Day   = readNumber(" Please enter a Day   : ");
+
+
+    stDate Date = initDate(Day, Month, Year);
+
+    printDate(Date);
+    printDate(decreaseDateByXDecades(Date, readNumber("How many decades you want to sub ? : ")));
+
+    return (0);
+}
