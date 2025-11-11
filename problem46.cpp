000a7bd47cbef7a710ad2dd7536f3695e45e87e8
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-11 17:11:06 +0100

    decrease date by one millennium

---
+++ problem46.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+struct stDate
+{
+    short int Day;
+    short int Month;
+    short int Year;
+};
+
+stDate initDate(short int Day, short int Month, short int Year)
+{
+    stDate Date;
+
+    Date.Day    =   Day;
+    Date.Month  =   Month;
+    Date.Year   =   Year;
+
+    return (Date);
+}
+
+void printDate(stDate Date)
+{
+    std::cout << Date.Day << "/" << Date.Month << "/" << Date.Year << std::endl;
+}
+
+int readNumber(std::string msg)
+{
+    int Number;
+
+    std::cout << msg;
+    std::cin >> Number;
+
+    return (Number);
+}
+
+stDate decreaseDateByOneMillennium(stDate Date)
+{
+    Date.Year -= 1000;
+
+    return (Date);
+}
+
+int main(void)
+{
+    short int Year  = readNumber(" Please enter a Year  : ");
+    short int Month = readNumber(" Please enter a Month : ");
+    short int Day   = readNumber(" Please enter a Day   : ");
+
+
+    stDate Date = initDate(Day, Month, Year);
+
+    printDate(Date);
+    printDate(decreaseDateByOneMillennium(Date));
+
+    return (0);
+}
