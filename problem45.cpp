6fa40f7e727fd1d0280dc5ef0da638ece8708c5e
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-11 17:13:38 +0100

    decrease date by one century

--- problem45.cpp
+++ problem45.cpp
@@ -33,28 +33,6 @@ int readNumber(std::string msg)
     return (Number);
 }
 
-bool leapYear(short int year)
-{
-    return ((year % 4 == 0) ? ((year % 100 == 0) ? ((year % 400 == 0) ? true : false ) : true) : false);
-}
-
-short int getMonthDays(short int Month, short int Year)
-{
-    short int listOfDays[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-    return ((Month == 2) ? (leapYear(Year) ? 29 : 28) : listOfDays[Month]); 
-}
-
-bool isLastDayInMonth(short int Day, short int Month, short int Year)
-{
-    return (Day == getMonthDays(Month, Year));
-}
-
-bool isLastMonth(short int Month)
-{
-    return (Month == 12);
-}
-
 stDate decreaseDateByOneCentury(stDate Date)
 {
     Date.Year -= 100;
