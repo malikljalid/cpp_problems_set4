2fb9039ce6dff3b8912ce95fdb7fdea6afaacd5f
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-13 00:33:38 +0100

    print date in given format

--- problem65.cpp
+++ problem65.cpp
@@ -50,19 +50,6 @@ int readNumber(std::string msg)
     return (Number);
 }
 
-std::string findAndReplaceIn(std::string msg, std::string toFind, std::string toReplace)
-{
-    int i = msg.find(toFind);
-
-    while (i != (int)std::string::npos)
-    {
-        msg.replace(i, toFind.length(), toReplace);
-        i = msg.find(toFind);
-    }
-
-    return (msg);
-}
-
 bool leapYear(short int year)
 {
     return ((year % 4 == 0) ? ((year % 100 == 0) ? ((year % 400 == 0) ? true : false ) : true) : false);
