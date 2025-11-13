f2e1688f17a634de405fc6611b4b60b19068040b
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-13 16:13:21 +0100

    add safe input and input handling library to project

--- project1.cpp
+++ project1.cpp
@@ -5,3 +5,6 @@
 #include <vector>
 #include <limits>
 
+enum enMenuOptions  { LIST=1, ADD, DELETE, UPDATE, FIND, TRANSACTION, EXIT };
+enum enTransactions { DEPOSITE=1, WITHDRAW, BALANCES, BACKMENU };
+
---
+++ safeInput.hpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <stdexcept>
+
+namespace safeInput
+{
+    void clearInput(void)
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    int getInt(const std::string &prompt)
+    {
+        int value = -404;
+
+        while (true)
+        {
+            std::cout << prompt;
+            std::cin >> value;
+
+            if (!std::cin.fail())
+            {
+                clearInput();
+                return (value);
+            }
+
+            std::cout << "Invalid integer input! Please try again...\n";
+            clearInput();
+        }
+    }
+
+    double getDouble(const std::string &prompt)
+    {
+        double value = -404.404;
+
+        while (true)
+        {
+            std::cout << prompt;
+            std::cin >> value;
+
+            if (!std::cin.fail())
+            {
+                clearInput();
+                return (value);
+            }
+
+            std::cout << "Invalid double input! Please try again...\n";
+            clearInput();
+        }
+    }
+
+    std::string getString(const std::string &prompt)
+    {
+        std::string value = "error 404";
+
+        std::cout << prompt;
+        getline(std::cin, value);
+
+        return (value);
+    }
+}
