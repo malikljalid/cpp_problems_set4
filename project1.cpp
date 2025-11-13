9eccf581cb4580a824bc99e1b0ed2cac4da26beb
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-13 16:14:07 +0100

    include safe input library in the project

--- project1.cpp
+++ project1.cpp
@@ -4,7 +4,36 @@
 #include <string>
 #include <vector>
 #include <limits>
+#include "safeInput.hpp"
 
 enum enMenuOptions  { LIST=1, ADD, DELETE, UPDATE, FIND, TRANSACTION, EXIT };
 enum enTransactions { DEPOSITE=1, WITHDRAW, BALANCES, BACKMENU };
+
+struct stListFile
+{
+    std::string Name;
+    std::string Delim;
+};
+
+struct stBankRecord
+{
+    std::string Name;
+    std::string PhoneNumber;
+    std::string AccountNumber;
+    short int   PinCode;
+    short int   Balance;
+};
+
+struct stOperation
+{
+    enMenuOptions   Basic;
+    enTransactions  Transaction;
+};
+
+struct stBank
+{   
+    stListFile                      ListFile;
+    std::vector <stBankRecord>      vListVector;
+    stOperation                     Operation;
+};
 
