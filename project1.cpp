9349bf633be8c712c1088c26008b1ecd10bd1f38
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-13 17:28:03 +0100

    implement read data methods for the project

--- project1.cpp
+++ project1.cpp
@@ -20,8 +20,8 @@ struct stBankRecord
     std::string Name;
     std::string PhoneNumber;
     std::string AccountNumber;
-    short int   PinCode;
-    short int   Balance;
+    int   PinCode;
+    int   Balance;
 };
 
 struct stOperation
@@ -36,4 +36,77 @@ struct stBank
     std::vector <stBankRecord>      vListVector;
     stOperation                     Operation;
 };
+
+stBankRecord readBankRecord(void)
+{
+    stBankRecord BR;
+
+    BR.Name = safeInput::getString("Please enter your name : ");
+
+    BR.PhoneNumber = safeInput::getString("Please enter your Phone number : ");
+
+    BR.AccountNumber = safeInput::getString("Please enter your Account Number : ");
+
+    BR.PinCode = safeInput::getInt("Please enter your PinCode : ");
+
+    BR.Balance = safeInput::getInt("Please enter your Balance : ");
+
+    return (BR);
+}
+
+stBankRecord readBankRecord(const std::string &ClientID)
+{
+    stBankRecord BR;
+
+    BR.Name = safeInput::getString("Please enter your name : ");
+
+    BR.PhoneNumber = safeInput::getString("Please enter your Phone number : ");
+
+    BR.AccountNumber = ClientID;
+
+    std::cout << "Please enter your PinCode : ";
+    std::cin >> BR.PinCode;
+
+    std::cout << "Please enter your Balance : ";
+    std::cin >> BR.Balance;
+
+    return (BR);
+}
+
+std::string readAccountNumber(void)
+{
+    return (safeInput::getString("Please enter an account number : "));
+}
+
+enMenuOptions readUserOperation(void)
+{
+    return ((enMenuOptions)safeInput::getInt("\nChoose what do you want to do [1 ~ 7] ? : "));
+}
+
+enTransactions readUserTransaction(void)
+{
+    return ((enTransactions)safeInput::getInt("\nChoose what do you want to do [1 ~ 4] ? : "));
+}
+
+int readDepositeAmount(void)
+{
+    return (safeInput::getInt("Please enter the amount to deposite : "));
+}
+
+int readWithdrawAmount(short int Balance)
+{
+    int WithdrawAmount;
+
+    do
+    {
+        WithdrawAmount = safeInput::getInt("Please enter the amount to withdraw : ");
+
+        if (WithdrawAmount > Balance)
+            std::cout << "\nAmount exceeds balance, you can withdraw up to " << Balance << "\n\n";
+    }
+    while (WithdrawAmount > Balance);
+
+    return (WithdrawAmount);
+}
+
 
